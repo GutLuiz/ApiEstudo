2bf4e720325d9b204bd075198102c0f0de8f73f8
Luiz Gustavo <gutfoda@gmail.com>
2024-10-28 10:59:52 -0300

    UseEffect conceitos

--- conceitos/UseState_UseEffect.js
+++ conceitos/UseState_UseEffect.js
@@ -25,3 +25,42 @@ function Counter() {
 }
 
 // Neste exemplo, count é o estado que armazena o número de cliques, e setCount é a função que atualiza o valor de count.
+
+
+/*
+    -> UseEffect: É um hook que permite lidar com efeitos colaterais em componentes funcionais. Ele é executado apos o render 
+    do componente e é util para lidar com operações assíncronas, como chamadas de API, manipulação do DOM, timers, entre outros.
+*/
+
+//EXEMPLO:
+
+useEffect(() => {
+   
+    return () => {
+    
+    };
+  }, [dependencies]);
+
+//Efeito: é o codigo que você deseja executar apos o render do componente
+//Limpeza: é a função opcional que limpa o efeito quando o componente é desmontado ou quando as dependencias mudam
+//Dependencias: É um array que contem valores que, quando mudam, fazem o useEffect ser executado novamente. Quando vazio([]), o efeito executado apenas uma vez, no primeiro render.
+  
+//Exemplo 2:
+import React, { useState, useEffect } from 'react';
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval); 
+  }, []);
+
+  return <p>Segundos: {seconds}</p>;
+}
+/*  Neste exemplo, useEffect cria um intervalo de 1 segundo para atualizar o estado seconds.
+    A função de limpeza (clearInterval) é usada para limpar o intervalo quando o componente for desmontado.
+*/
