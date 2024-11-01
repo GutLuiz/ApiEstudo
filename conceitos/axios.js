4b719da9b6bfe9097f54607e6f785af18670bda0
Luiz Gustavo <gutfoda@gmail.com>
2024-11-01 10:50:33 -0300

    Comentando todo o codigo da requisição com axios

--- conceitos/axios.js
+++ conceitos/axios.js
@@ -4,16 +4,16 @@
     e é muito usado em aplicações front-end para interagir com APIs.
 */
 // Exemplo:
-import axios from "axios";
+import axios from "axios"; // importação do axios
 
-export const registerUser = async (userData) => {
-  try {
-    const response = await axios.post(
-      "https://localhost:7013/api/v1/register",
-      userData
+export const registerUser = async (userData) => { // Export : para a função poder exportar para outros arquivos, colocando uma variavel chamada registerUser fazendo uma funçao assincrona, colocando em parametro o userdata
+  try { // abrindo um try
+    const response = await axios.post( // fazendo uma const chamada response, nela tem o await que espera a resolução de uma promesssa, em seguida colocamos axios + o metodo. 
+      "https://localhost:7013/api/v1/register", // coloca  a url da API que esta te fornecendo o back end
+      userData // Coloca o userData para armazenar as informações que o usuario colocou.
     );
-    return response.data;
-  } catch (error) {
+    return response.data; // Retorna as informações
+  } catch (error) { // fazendo um catch
     console.error("Erro ao registrar usuário:", error);
     throw error;
   }
